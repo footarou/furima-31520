a89d63b5abd9a8175f896652a33700d3da539471
footarou <footarou@gmail.com>
2021-03-15 21:44:21 +0900

    Item Edit function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,10 +19,18 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def edit
-  # end
+  def edit
+  end
 
   def show
+  end
+
+  def update
+    @item.update(item_params)
+    redirect_to item_path
+  end
+
+  def destroy
   end
 
   private
