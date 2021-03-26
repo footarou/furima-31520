cdcf30151ca0f152824232ae0c68b71afe92e661
footarou <footarou@gmail.com>
2021-03-26 13:08:18 +0900

    Item Purchase function revised#4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :users, only: [:show]
   resources :items do
     resources :orders, only: [:index, :create]
   end
